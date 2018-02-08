2eaf7c342d9a05abf927858036b33a61826442cc
Alexander Sgibnev <sae762@gmail.com>
2018-02-08 22:03:08 +0600

    release 1.0

---
+++ README.md
@@ -0,0 +1,7 @@
+# actual code for my out1.lan
+
+- outdoor thermometer am2320 in dht22 onewire mode
+- light sensor, digital for now
+- pir motion sensor
+- led tape control
+- rpc for all of this (in progress)
---
+++ fs/init.js
@@ -0,0 +1,106 @@
+load('api_config.js');
+load('api_gpio.js');
+load('api_mqtt.js');
+load('api_net.js');
+load('api_sys.js');
+load('api_timer.js');
+load('api_dht.js');
+load('api_rpc.js');
+
+let led = 2;//Cfg.get('pins.led');
+
+let getInfo = function() {
+  return JSON.stringify({
+    total_ram: Sys.total_ram(),
+    free_ram: Sys.free_ram()
+  });
+};
+
+GPIO.set_mode(led, GPIO.MODE_OUTPUT);
+let mydht = DHT.create(12, DHT.DHT22);
+let tmp=0;
+let hum=0;
+
+Timer.set(3000 /* 1 sec */, true /* repeat */, function() {
+  GPIO.write(led,1);
+  tmp=mydht.getTemp();
+  hum=mydht.getHumidity();
+  //if (isNaN(tmp) || isNaN(hum)) GPIO.write(led,0);
+}, null);
+
+RPC.addHandler('t', function(args) {
+    return Math.round(tmp);
+});
+
+RPC.addHandler('h', function(args) {
+    return Math.round(hum);
+});
+
+/**
+ * Motion detector block
+ * */
+let mov_pin = 14; //movement sensor pin
+let led_tape=5; //light control pin
+let mvm=0; //movement detected
+let led_tape_disabled=0; //disable led light (if day f/ex)
+
+GPIO.set_mode(mov_pin, GPIO.MODE_INPUT);
+GPIO.set_pull(mov_pin, GPIO.PULL_NONE); //no pull down in esp8266
+GPIO.set_mode(led_tape, GPIO.MODE_OUTPUT);
+GPIO.write(led_tape,0);
+//by timer, not by interrupt, so we can make some time-based handling: delay or slow dimming if use pwm
+Timer.set(500 , true , function() {
+  mvm=GPIO.read(mov_pin);
+  if(mvm===1 && led_tape_disabled===0) {  
+    GPIO.write(led_tape,1);
+  } else {
+    GPIO.write(led_tape,0);
+  }
+}, null);
+
+RPC.addHandler('mvm', function(args) {
+    //handle request { "disable":"1" }
+    if (typeof(args) === 'object' && typeof(args.disable) === 'number') {
+      led_tape_disabled=args.disable;
+    }
+    return mvm;
+});
+
+/**
+ * Light sensor block
+ * */
+let light_pin=16; //light sensor digital pin
+GPIO.set_mode(light_pin, GPIO.MODE_INPUT);
+GPIO.set_pull(light_pin, GPIO.PULL_UP); //no pull down in esp8266
+
+let light=0;//0 - darkness, 1 - daylight
+
+Timer.set(2000 , true , function() {
+  if (GPIO.read(light_pin)!==0) {
+    light=0;
+    led_tape_disabled=0; //control led tape here
+  } else {
+    light=1;
+    led_tape_disabled=1; //control led tape here
+  }
+}, null);
+
+//interrupt doesnt work for me here :-\
+/*
+GPIO.set_int_handler(light_pin, GPIO.INT_EDGE_ANY, function(x) {
+  if (GPIO.read(light_pin)!==0) {
+    GPIO.write(led,1);
+    light=0;
+  } else {
+    GPIO.write(led,0);
+    light=1;
+  }
+}, null);
+GPIO.enable_int(light_pin);
+*/
+
+RPC.addHandler('light', function(args) {
+    return light;
+});
+ 
+ 
